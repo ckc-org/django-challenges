5b8b6ed5a7f864bb668cc97d175200aa209b7e53
Logan-Ruf <cloganruf@gmail.com>
2021-07-19 11:31:57 -0700

    One liner

--- query_reduction_challenge/src/backend/apps/users/views.py
+++ query_reduction_challenge/src/backend/apps/users/views.py
@@ -44,5 +44,5 @@ class UserViewSet(
     GenericViewSet,
 ):
     permission_classes = []
-    queryset = User.objects.all()
+    queryset = User.objects.all().prefetch_related('companies', 'companies__addresses')
     serializer_class = UserSerializer
