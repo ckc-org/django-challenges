64a70bf6d165c049eac66028b3fca802df75f7a6
Bailey Lind-Trefts <gibson.bailey@live.com>
2021-07-21 20:06:07 -0700

    Add script to determine the most likely challenge directory.

---
+++ determine_challenge_directory.py
@@ -0,0 +1,26 @@
+import sys
+from collections import defaultdict
+
+
+BLACKLIST_DIRS = {
+    '.github',
+}
+
+directories = defaultdict(int)
+
+for line in sys.stdin:
+    line = line.strip()
+    if '/' not in line:
+        continue
+    directory = line.split('/')[0]
+    if directory not in BLACKLIST_DIRS:
+        directories[directory] += 1
+
+maxdir = None
+max_frequency = 0
+for dir in directories:
+    if directories[dir] > max_frequency:
+        max_dir = dir
+        max_frequency = directories[dir]
+
+print(max_dir)
